d9eb3230c3ebe904013f970cb6c06dac31098322
Anthony Santi <darkstars31@gmail.com>
2023-05-10 14:34:44 -0500

    forcing a deploy

--- src/pages/player.tsx
+++ src/pages/player.tsx
@@ -101,6 +101,7 @@ export function Player() {
     const nth = (n: string | number) => {
         return ["st", "nd", "rd"][((Number(n) + 90) % 100 - 10) % 10 - 1] || "th";
     };
+    
   
     return (
         <Container>
