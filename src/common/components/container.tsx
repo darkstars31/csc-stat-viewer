61af055a56ab6054b0121a8c4a71c036567d0d17
Anthony Santi <darkstars31@gmail.com>
2023-07-06 08:56:23 -0500

    Removed 'container' class from Container component to fix mobile widths

--- src/common/components/container.tsx
+++ src/common/components/container.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 
 export function Container( { children }: { children: React.ReactNode | React.ReactNode[]} ) {
     return (
-        <section className="container h-screen py-16 mx-auto">
+        <section className="h-screen py-16 mx-auto">
             <div className="sm:max-w-screen-xl px-4 sm:px-6 lg:px-8">
                 {children}
             </div>
