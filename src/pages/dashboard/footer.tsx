6d2a46e4fa9105a4894f7b19c08df9e0cc161677
jessemh <regr3tti@gmail.com>
2023-07-07 15:09:24 -0400

    removed data-te references

--- src/pages/dashboard/footer.tsx
+++ src/pages/dashboard/footer.tsx
@@ -11,8 +11,7 @@ export function DashboardFooter() {
                 type="button"
                 target="_blank"
                 className="m-1 h-9 w-9 rounded-full border-2 border-white uppercase leading-normal text-white transition duration-150 ease-in-out hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0"
-                data-te-ripple-init
-                data-te-ripple-color="light" rel="noreferrer">
+                rel="noreferrer">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   className="mx-auto h-full w-4"
