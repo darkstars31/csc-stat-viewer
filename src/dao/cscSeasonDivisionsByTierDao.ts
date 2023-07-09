81e9ec3b25f4d4eba1130664b2850eb5956fd03d
Anthony Santi <darkstars31@gmail.com>
2023-07-08 22:28:40 -0500

    WIP

---
+++ src/dao/cscSeasonDivisionsByTierDao.ts
@@ -0,0 +1,53 @@
+import { useQuery, UseQueryResult } from "@tanstack/react-query";
+import { appConfig } from "../dataConfig";
+import { ConferencesQuery, Standing } from "../models/cscSeasonDivisionByTier";
+
+const OneHour = 1000 * 60 * 60;
+
+const fetchGraph = async ( season?: number ) => await fetch(appConfig.endpoints.cscGraphQL.core,
+    { method: "POST", 
+        body: JSON.stringify({
+            "operationName": "",
+            "query": `query Seasons( $season: Int) {
+                season(number: $season) {
+                    standings {
+                        tier {
+                            name
+                        }
+                        divisions {
+                            name
+                            teams {
+                                team {
+                                    name
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            `,
+            "variables": {
+                "season": season,
+            }      
+        }),
+        headers: {
+            'Content-Type': "application/json"
+        }
+    })
+    .then( async response => {
+        return response.json().then( (json: ConferencesQuery) => {
+            return json.data.season.standings;
+        });
+    } );
+
+
+
+export function useCscSeasonDivisionsByTier( season?: number ): UseQueryResult<Standing[]> {
+    return useQuery( 
+        [`cscSeasonDivisionsByTier-${season}-graph`], 
+        () => fetchGraph(season), 
+        {
+            staleTime: OneHour,
+        }
+    );
+}
