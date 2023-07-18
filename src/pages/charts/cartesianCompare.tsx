032a4f9b9bef49c34b87d4e0c7f0245c60570ee3
Anthony Santi <darkstars31@gmail.com>
2023-07-18 12:38:45 -0500

    Checkbox for HLTV Rating on Rating/MMR chart

--- src/pages/charts/cartesianCompare.tsx
+++ src/pages/charts/cartesianCompare.tsx
@@ -2,28 +2,38 @@ import * as React from "react";
 import { getTotalPlayerAverages } from "../../common/utils/player-utils";
 import { Player } from "../../models";
 import ReactECharts from "echarts-for-react";
-import { linearRegression, linearRegressionLine } from "simple-statistics";
+import { linearRegression, linearRegressionLine, max, min } from "simple-statistics";
 
 type Props = {
     playerData?: Player[];
 }
 
 export function CartesianCompare( { playerData = [] }: Props) {
+    const [ isHltvRating, setIsHltvRating ] = React.useState(false);
 
-    const data = playerData.map( p => ( [p.stats?.rating, p.mmr, p.tier.name, p.name]));
+    const getPreferredRating = ( p: Player ) => isHltvRating ? p.hltvTwoPointO! : p.stats?.rating 
+
+    const data = playerData.map( p => ( [ getPreferredRating(p), p.mmr, p.tier.name, p.name, p]));
     const playerTotalStats = getTotalPlayerAverages(playerData ?? []);
 
-    const ratingMmrPairs = playerData.map( p => ( [p.stats.rating!, p.mmr!]));
+    const ratingMmrPairs = playerData.map( p => ( [ getPreferredRating(p), p.mmr!]));
     const linearRegressionFunction = linearRegression( ratingMmrPairs );
     const linearRegressionLineFunction = linearRegressionLine( linearRegressionFunction );
 
-    const highest = playerTotalStats.highest.rating ? Number((playerTotalStats?.highest.rating + 0.05).toFixed(2)) : 0;
-    const lowest = playerTotalStats.lowest.rating ? Number((playerTotalStats?.lowest.rating - 0.05).toFixed(2)) : 0;
+    const xmin = isHltvRating ? min(playerData.map( p => p.hltvTwoPointO!)) : Number((playerTotalStats?.lowest.rating).toFixed(2));
+    const xmax =  isHltvRating ? max(playerData.map( p => p.hltvTwoPointO!)): Number((playerTotalStats?.highest.rating).toFixed(2));
+
+    const highest = playerTotalStats.highest.rating ? xmax + 0.05 : 0;
+    const lowest = playerTotalStats.lowest.rating ? xmin - 0.05  : 0;
 
     const seriesSettings = ( datasetIndex: number) => ({
         symbolsize: 60,
         datasetIndex: datasetIndex,
         type: "scatter",
+        symbolSize: 8,
+        select: {
+          selectedMode: 'multiple',
+        },
         emphasis: {
             focus: 'series',
             label: {
@@ -35,6 +45,19 @@ export function CartesianCompare( { playerData = [] }: Props) {
                 position: 'top',
             }
           },
+        tooltip: {
+          show: true,
+          triggerOn: 'none',
+          showContent: true,
+          alwaysShowContent: true,
+          enterable: true,
+          renderMode: 'html',
+          backgroundColor: 'rgba(10,10,10,1)',
+          position: ['11%','11%'],
+          formatter: function (param: { data: any[]; }) {
+              return `<b>${param.data[3]}</b><br />${isHltvRating ? 'HLTV Rating' : 'Rating'}: ${param.data[0].toFixed(2)}<br />MMR: ${param.data[1]}`;
+          }
+      }
     });
 
     const optionRatingMMR = {
@@ -127,23 +150,17 @@ export function CartesianCompare( { playerData = [] }: Props) {
               }
         },
         tooltip: {
-            show: true,
+            show: false,
+            trigger: 'item',
             label: {
                 formatter: function (param: { data: any[]; }) {
-
-                    return `${param.data[3]}`;
+                    console.info(param);
+                    return `${param.data[3]} ${param.data[2]}`;
                 },
             }
         },
         series: [ { 
             name: "Recruit", ...seriesSettings(1),
-            // tooltip: {
-            //     show: true,
-            //     position: ['50%','50%'],
-            //     formatter: function (param: { data: any[]; }) {
-            //         return `${param.data[3]}<br />Rating: ${param.data[0].toFixed(2)}<br />MMR: ${param.data[1]}`;
-            //     }
-            // }
         },
         { 
             name: "Prospect", ...seriesSettings(2),
@@ -180,7 +197,20 @@ export function CartesianCompare( { playerData = [] }: Props) {
     };
 
     return (
+      
         <div className='py-4'>
+          <input
+                        className="relative float-left -ml-[1.5rem] mr-[6px] mt-[0.15rem] h-[1.125rem] w-[1.125rem] appearance-none rounded-[0.25rem] border-[0.125rem] border-solid border-neutral-300 outline-none before:pointer-events-none before:absolute before:h-[0.875rem] before:w-[0.875rem] before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_13px_transparent] before:content-[''] checked:border-primary checked:bg-primary checked:before:opacity-[0.16] checked:after:absolute checked:after:-mt-px checked:after:ml-[0.25rem] checked:after:block checked:after:h-[0.8125rem] checked:after:w-[0.375rem] checked:after:rotate-45 checked:after:border-[0.125rem] checked:after:border-l-0 checked:after:border-t-0 checked:after:border-solid checked:after:border-white checked:after:bg-transparent checked:after:content-[''] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:shadow-none focus:transition-[border-color_0.2s] focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-[0.875rem] focus:after:w-[0.875rem] focus:after:rounded-[0.125rem] focus:after:content-[''] checked:focus:before:scale-100 checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:after:-mt-px checked:focus:after:ml-[0.25rem] checked:focus:after:h-[0.8125rem] checked:focus:after:w-[0.375rem] checked:focus:after:rotate-45 checked:focus:after:rounded-none checked:focus:after:border-[0.125rem] checked:focus:after:border-l-0 checked:focus:after:border-t-0 checked:focus:after:border-solid checked:focus:after:border-white checked:focus:after:bg-transparent dark:border-neutral-600 dark:checked:border-primary dark:checked:bg-primary dark:focus:before:shadow-[0px_0px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca]"
+                        type="checkbox"
+                        value=""
+                        onChange={ () => setIsHltvRating(!isHltvRating) }
+                        id="checkboxChecked"
+                        checked={ isHltvRating } />
+                    <label
+                        className="inline-block pl-[0.15rem] hover:cursor-pointer"
+                        htmlFor="checkboxChecked">
+                        Use HLTV Rating
+                    </label>
             <ReactECharts option={optionRatingMMR} style={{height: 600}} />
         </div>
     );
