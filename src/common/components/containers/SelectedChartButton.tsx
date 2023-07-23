79c7f91a0eabf132e86ef7b7c2125e8d0b79012b
jessemh <regr3tti@gmail.com>
2023-07-23 18:27:06 -0400

    Updates to responsiveness and spacing

--- src/common/components/containers/SelectedChartButton.tsx
+++ src/common/components/containers/SelectedChartButton.tsx
@@ -9,7 +9,7 @@ export function SelectedChartButton({ children, onClick }: Props) {
   return (
     <button
       onClick={onClick}
-      className="bg-midnight1 text-white border border-blue-500 px-4 py-1 mx-1 rounded-lg"
+      className="bg-midnight1 text-white border border-blue-500 px-4 py-2 mx-1 rounded-lg"
     >
       {children}
     </button>
--- src/common/components/containers/StandardBoxRow.tsx
+++ src/common/components/containers/StandardBoxRow.tsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export function StandardBoxRow({ children }: { children: React.ReactNode }) {
   return (
-    <section className="grid grid-cols-1 gap-2 lg:grid-cols-2">{children}</section>
+    <section className="grid grid-cols-1 gap-2 md:grid-cols-2">{children}</section>
   );
 }
