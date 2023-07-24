d893ef86eca0533bd6ab15b69e4fbe7849311b4a
jessemh <regr3tti@gmail.com>
2023-07-23 23:26:55 -0400

    Added padding to thinbox

--- src/common/components/containers/StandardContentThinBox.tsx
+++ src/common/components/containers/StandardContentThinBox.tsx
@@ -6,7 +6,7 @@ export function StandardContentThinBox({
     children: React.ReactNode;
   }) {
     return (
-      <section className="place-content-center mb-3 flex flex-col sm:flex-row bg-midnight1 rounded-lg shadow-md shadow-black/20 dark:shadow-black/40">
+      <section className="place-content-center mb-3 p-2 flex flex-col sm:flex-row bg-midnight1 rounded-lg shadow-md shadow-black/20 dark:shadow-black/40">
         {children}
       </section>
     );
