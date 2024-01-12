fc55e541c5466dd9457bce9c3e04a56ff9dda816
Anthony Santi <darkstars31@gmail.com>
2024-01-12 13:55:41 -0600

    Replaced MMR visual logic with if 0 show ???

--- src/common/components/mmr.tsx
+++ src/common/components/mmr.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { useDataContext } from "../../DataContext";
 import { Player } from "../../models/player";
 import { Player as FranchisePlayer } from "../../models/franchise-types";
 
@@ -8,9 +7,5 @@ type Props = {
 }
 
 export function Mmr( { player }: Props) {
-    const { dataConfig } = useDataContext();
-    const seasonStartDate = new Date( dataConfig?.seasonStartDate ?? "");
-    const seasonEndDate = new Date( dataConfig?.seasonEndDate ?? "");
-    const isInSeason = seasonEndDate > new Date() && seasonStartDate < new Date();
-    return <span>{ isInSeason ? player.mmr : "???"}</span> ;
+    return <span>{ player.mmr === 0 ? player.mmr : "???"}</span> ;
 }
