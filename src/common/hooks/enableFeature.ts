becfaef0dc5e3c8de8ac836fc409754a224f7657
Anthony Santi <darkstars31@gmail.com>
2024-05-01 16:19:45 -0500

    Maybe fixed issue with access control

--- src/common/hooks/enableFeature.ts
+++ src/common/hooks/enableFeature.ts
@@ -10,7 +10,7 @@ export const useEnableFeature = ( featureString: string ) => {
 
     React.useEffect(() => {
         const rules = [
-            { name: "canUsePlayerComparison", function: () => isIn( loggedinUser?.team?.franchise.prefix, 'V0VU') || isIn( loggedinUser?.steam64Id, ['NzY1NjExOTgxNjk5MzM4NzA=','NzY1NjExOTc5ODcwMTU0NjA=','NzY1NjExOTgxMDcxNTY4MjA=','NzY1NjExOTgzMzg1NDg0MzA='])},
+            { name: "canUsePlayerComparison", function: () => isIn( loggedinUser?.team?.franchise.prefix, 'V0VU') || isIn( loggedinUser?.steam64Id, ['NzY1NjExOTgxNjk5MzM4Nzg=','NzY1NjExOTc5ODcwMTU0NjA=','NzY1NjExOTgxMDcxNTY4MjA=','NzY1NjExOTc5ODcwMTU0NTg='])},
         ];
     
         const rule = rules.find( r => r.name === featureString );
