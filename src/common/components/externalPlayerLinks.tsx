f5e1130330f3f41a095ba4961346358699bb6878
Anthony Santi <darkstars31@gmail.com>
2024-05-02 10:55:02 -0500

    Added leetify links to external player links

--- src/common/components/externalPlayerLinks.tsx
+++ src/common/components/externalPlayerLinks.tsx
@@ -14,10 +14,11 @@ export function ExternalPlayerLinks( { player }: Props) {
     const { data: faceitSearchPlayer = undefined } = useFetchFaceItPlayerWithCache( player );
     const faceitPlayerProfile = faceitSearchPlayer?.faceit_name;
     return (
-    <div className="lg:flex">
-        { player?.name && <div className="hover:cursor-pointer bg-slate-700 mx-1 p-1 rounded w-6 float-left m-[1px]"><a href={`https://csconfederation.com/stats/profile/${player.name}`} target="_blank" rel="noreferrer"><img src={cscLogo} alt=""/></a></div>}
-        { player?.steam64Id && <div className="hover:cursor-pointer bg-slate-700 mx-1 p-1 rounded w-6 float-left m-[1px]"><a href={`http://steamcommunity.com/profiles/${player.steam64Id}`} target="_blank" rel="noreferrer"><FaSteamSquare /></a></div>}
-        { player?.discordId && <div className="hover:cursor-pointer bg-blue-700 mx-1 p-1 rounded w-6 float-left m-[1px]"><a href={`https://discordapp.com/users/${player.discordId}`} target="_blank" rel="noreferrer"><RxDiscordLogo /></a></div> }
-        { faceitPlayerProfile && <div className="hover:cursor-pointer text-orange-500 mx-1 bg-slate-900 p-1 rounded w-6 float-left m-[1px]"><a href={`https://www.faceit.com/en/players/${faceitPlayerProfile}`} target="_blank" rel="noreferrer"><SiFaceit /></a></div> }
+    <div className="lg:flex flex-wrap gap-1">
+        { player?.name && <div className="hover:cursor-pointer bg-slate-700 p-1 rounded w-6 float-left m-[1px]"><a href={`https://csconfederation.com/stats/profile/${player.name}`} target="_blank" rel="noreferrer"><img src={cscLogo} alt=""/></a></div>}
+        { player?.steam64Id && <div className="hover:cursor-pointer bg-slate-700 p-1 rounded w-6 float-left m-[1px]"><a href={`https://leetify.com/app/profile/${player.steam64Id}`} target="_blank" rel="noreferrer"><img className="pt-[0.1em]" src="https://leetify.com/assets/leetify/leetify-icon.svg" alt=""/></a></div>}
+        { player?.steam64Id && <div className="hover:cursor-pointer bg-slate-700 p-1 rounded w-6 float-left m-[1px]"><a href={`http://steamcommunity.com/profiles/${player.steam64Id}`} target="_blank" rel="noreferrer"><FaSteamSquare /></a></div>}
+        { player?.discordId && <div className="hover:cursor-pointer bg-blue-700 p-1 rounded w-6 float-left m-[1px]"><a href={`https://discordapp.com/users/${player.discordId}`} target="_blank" rel="noreferrer"><RxDiscordLogo /></a></div> }
+        { faceitPlayerProfile && <div className="hover:cursor-pointer text-orange-500 bg-slate-900 p-1 rounded w-6 float-left m-[1px]"><a href={`https://www.faceit.com/en/players/${faceitPlayerProfile}`} target="_blank" rel="noreferrer"><SiFaceit /></a></div> }
     </div>);
 };
