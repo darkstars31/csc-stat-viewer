fd07d076c563d145659f26fe43926e9a09031398
jessemh <regr3tti@gmail.com>
2024-05-16 19:22:58 -0400

    Updates

--- src/pages/leaderboards.tsx
+++ src/pages/leaderboards.tsx
@@ -3,9 +3,13 @@ import { Container } from "../common/components/container";
 import { Loading } from "../common/components/loading";
 import { useDataContext } from "../DataContext";
 import Select, { SingleValue } from "react-select";
-import * as Containers from "../common/components/containers";
-import * as aslb from "./charts/allStatsLeaderBoards";
-//import _sort from "lodash/sortBy";
+import { StandardBackgroundPage } from "../common/components/containers/StandardBackgroundPage";
+import { ChartButtonBoundingBox } from "../common/components/containers/ChartButtonBoundingBox";
+import { WeaponLeaderboards } from "./leaderboards/weapons";
+import { useLocation } from "wouter";
+import { Chickens } from "./leaderboards/chickens";
+import { GeneralLeaderBoards } from "./leaderboards/general";
+import { selectClassNames } from "../common/utils/select-utils";
 
 export function LeaderBoards() {
     const qs = new URLSearchParams(window.location.search);
@@ -20,7 +24,6 @@ export function LeaderBoards() {
     
     const playerData = filterBy?.value.includes("All") ? player : player.filter( f => f.tier.name.toLowerCase() === filterBy?.value.toLowerCase());
 
-
     const selectClassNames = {
         placeholder: () => "text-gray-400 bg-inherit",
         container: () => "m-1 rounded bg-inherit",
@@ -34,6 +37,8 @@ export function LeaderBoards() {
         multiValueRemove: () => "text-slate-800 pl-1",
         singleValue: () => "text-slate-200",
     };
+
+    const tierButtonClass = "px-6 pb-2 pt-2.5 text-sm font-medium uppercase leading-normal transition duration-150 ease-in-out hover:bg-blue-400 focus:bg-blue-400 focus:outline-none focus:ring-0 active:bg-blue-300";
 
     const tierCounts = {
         premier: player.filter(f => f.tier.name.toLowerCase() === "premier").length,
@@ -54,9 +59,23 @@ export function LeaderBoards() {
         { label: `Recruit (${tierCounts.recruit})`, value: "Recruit", isDisabled: !tierCounts.recruit },
     ];
 
+    const showLimitOptionsList = [
+        { label: `5`, value: "5"},
+        { label: `10`, value: "10"},
+        { label: `20`, value: "20"},
+    ];
+
+    const pages = [
+        { name: "General", color: 'yellow'},
+        { name: "Extended", color: 'cyan'},
+        { name: "Weapons", color: 'green'},
+        { name: "All Stats", color: 'blue'}
+    ];
+
     if( loading.isLoadingCscPlayers ) {
         return <Container><Loading /></Container>;
     }
+
     return (
         <Container>
             <div className="mx-auto max-w-lg text-center">
@@ -66,8 +85,60 @@ export function LeaderBoards() {
                 </p>
             </div>
             <hr className="h-px my-4 bg-gray-200 border-0 dark:bg-gray-800" />
-            <Containers.StandardBackgroundPage>
-                <Containers.ChartButtonBoundingBox>
+            <div
+                className="justify-center flex flex-wrap rounded-md"
+                role="group">
+                {
+                    pages.map( page => 
+                        <button key={page.name} 
+                            type="button" 
+                            onClick={() => { 
+                                setLocation( window.location.pathname + '?q=' + page.name.toLowerCase()); 
+                                setSelectedPage(page.name.toLowerCase())
+                            }}
+                            className={`rounded-md ${page.name.toLowerCase() === selectedPage.toLowerCase() ? 'bg-blue-500' : 'bg-slate-700'} text-${page.color}-400 ${tierButtonClass} shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-blue-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-blue-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]`}
+                        >
+                            {page.name}
+                        </button>                     
+                    )
+                }                    
+            </div>
+            <div className="flex flex-box h-12 mx-auto justify-end">
+                <div className="basis-1/4">
+                            <div className="flex flex-row text-sm m-2">
+                                <label title="Order By" className="p-1 leading-9">
+                                    Show
+                                </label>
+                                <Select
+                                    className="grow"
+                                    unstyled              
+                                    defaultValue={showLimitOptionsList[0]}
+                                    isSearchable={false}
+                                    classNames={selectClassNames}
+                                    options={showLimitOptionsList}
+                                    onChange={( item ) => setLimit( parseInt(item!.value) )}
+                                />
+                        </div>
+                    </div>
+                    <div className="basis-1/4">
+                            <div className="flex flex-row text-sm m-2">
+                                <label title="Order By" className="p-1 leading-9">
+                                    Tier
+                                </label>
+                                <Select
+                                    className="grow"
+                                    unstyled              
+                                    defaultValue={tierOptionsList[0]}
+                                    isSearchable={false}
+                                    classNames={selectClassNames}
+                                    options={tierOptionsList}
+                                    onChange={setFilterBy}
+                                />
+                        </div>
+                    </div>
+                </div>
+            <StandardBackgroundPage>
+                <ChartButtonBoundingBox>
                     <div className="basis-1/2">
                             <div className="flex flex-row text-sm m-2">
                                 <label title="Order By" className="p-1 leading-9">
@@ -84,13 +155,27 @@ export function LeaderBoards() {
                                 />
                         </div>
                     </div>
-                </Containers.ChartButtonBoundingBox>
-                { playerData.length !== 0 &&
-                <>
-                <aslb.AllStatsLeaderboards playerData={playerData}/>
-                </>
-                }
-            </Containers.StandardBackgroundPage>
+                </ChartButtonBoundingBox>
+            </StandardBackgroundPage>
+            { playerData.length > 0 && 
+            <div className="pt-6">
+                <div className="flex flex-row flex-wrap gap-6">
+                    { selectedPage === "general" &&
+                        <GeneralLeaderBoards players={playerData} limit={limit} />
+                    }
+                    { selectedPage === "extended" && 
+                        <Chickens players={playerData} limit={limit} />
+                    }
+                    { selectedPage === "weapons" &&
+                        <WeaponLeaderboards players={playerData} limit={limit} />
+                    }
+                </div>   
+            </div> }
+            { !playerData.length && 
+                <div>
+                    No Players in this Tier for this stats sheet.
+                </div>
+            }
         </Container>
     );
 }
