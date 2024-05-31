9581439bc953aab19cb202063a687c0c74f60289
Anthony Santi <darkstars31@gmail.com>
2024-05-30 22:05:54 -0500

    Updated for season 14 regulation

--- src/dataConfig.tsx
+++ src/dataConfig.tsx
@@ -32,6 +32,13 @@ export const betaUsers = {
 export const dataConfiguration = [
     {
         season: 14,
+        seasonStartDate: "2024-05-30",
+        seasonEndDate: "2024-06-30",
+        name: "Season 14",
+        spreadsheetUrl: googleSheetsUrl("18qgpc4NdPNg4I5Q9lmzaEgUYyxHtq25JKZfTxiQg_lc",334898684),
+    },
+    {
+        season: 14,
         seasonStartDate: "2024-04-21",
         seasonEndDate: "2024-06-30",
         name: "Season 14 (Combines)",
