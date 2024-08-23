e2aa7587013369b38c4c1c8aeab386ffe089c06a
Anthony Santi <darkstars31@gmail.com>
2024-08-23 12:35:41 -0500

    Updated the Trades extended match history to include visual and updated styling

--- src/pages/player/extendedMatchHistoryTrades.tsx
+++ src/pages/player/extendedMatchHistoryTrades.tsx
@@ -24,16 +24,29 @@ export const ExtendedMatchHistoryTrades = ({ extendedMatchData } : { extendedMat
     console.info( trades );
 
     return (
-        <div className="flex flex-row flex-wrap gap-4 justify-center ">
-            {
-                  Object.entries(trades as any).map( ([key, value]: any) => (
+        <>
+            <div className="text-xs text-gray-500 text-center">*Trade Kills/Deaths over Total, does not currently account for opportunities.</div>
+            <div className="flex flex-row flex-wrap gap-4 justify-center ">
+                {
+                    Object.entries(trades as any).map( ([key, value]: any) => (
                     <div className="basis-1/6">
-                        <div className="font-bold">{key}</div>
-                        <div className="text-sm">K {value.tradeKill} / {value.totalKills} ({calculatePercentage(value.tradeKill, value.totalKills)}%)</div>
-                        <div className="text-sm">D {value.tradeDeath} / {value.totalDeaths} ({calculatePercentage(value.tradeDeath, value.totalDeaths)}%)</div>
+                        <div className="font-bold text-center">{key}</div>
+                        <div className="w-full bg-gray-200 rounded dark:bg-gray-700 m-1 h-6">
+                            <div className="float-left w-full text-center text-xs pt-1">Kills {value.tradeKill} / {value.totalKills} ({calculatePercentage(value.tradeKill, value.totalKills,0)}%) </div>
+                            <div className="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none h-6 rounded" 
+                            style={{width: `${calculatePercentage(value.tradeKill, value.totalKills)}%`}}>
+                                </div>
+                        </div>
+                        <div className="w-full bg-gray-200 rounded dark:bg-gray-700 m-1 h-6">
+                            <div className="float-left w-full text-center text-xs pt-1">Deaths {value.tradeDeath} / {value.totalDeaths} ({calculatePercentage(value.tradeDeath, value.totalDeaths,0)}%) </div>
+                            <div className="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none h-6 rounded" 
+                            style={{width: `${calculatePercentage(value.tradeDeath, value.totalDeaths)}%`}}>
+                                </div>
+                        </div>
                     </div>
-                   ))
-            }
-        </div>
+                    ))
+                }
+            </div>
+        </>
     )
 }
