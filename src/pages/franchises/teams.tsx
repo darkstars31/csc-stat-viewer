260e42bf904cab60c91cde08867e79a01cd17ade
Ethan Smith <8473137+ethsmith@users.noreply.github.com>
2024-09-24 11:54:21 -0500

    Add average rating to the main franchises page (#44)
    * Add average rating for the teams on main franchises page.
    
    * Add rating icon and place the rating somewhere else
    
    * Remove commented code

--- src/pages/franchises/teams.tsx
+++ src/pages/franchises/teams.tsx
@@ -3,6 +3,7 @@ import { Franchise } from "../../models/franchise-types";
 import { useDataContext } from "../../DataContext";
 import { PlayerTypes } from "../../common/utils/player-utils";
 import { GiPirateHat } from "react-icons/gi";
+import {BiStats} from "react-icons/bi";
 
 const tierNumber = {
     Recruit: 1,
@@ -28,55 +29,85 @@ export function FranchiseTeams({ franchise, selectedTier }: { franchise: Franchi
                         tierNumber[b.tier.name as keyof typeof tierNumber] -
                         tierNumber[a.tier.name as keyof typeof tierNumber],
                 )
-                .map(team => (
-                    <div key={`${team.tier.name}` }>
-                        <div className="basis-1/4 mx-4 border-b-[1px] border-slate-700 text-center">
-                            <strong>{team.name}</strong>{" "}
-                            <span
-                                className={`text-gray-400 italic text-${tiers?.find(item => item.tier.name === team.tier.name)?.tier.color ?? ""}-400`}
-                            >
-                                {team.tier.name}{" "}
-                            </span>
-                        </div>
-                        <div className="mx-4 px-2">
-                            { team.players.length > 0 ? team.players.map(player => {
-                                const playerWithStats = players.find(
-                                    p => p.steam64Id === player.steam64Id,
-                                );
-                                const isInactiveReserve =
-                                    playerWithStats?.type === PlayerTypes.INACTIVE_RESERVE;
+                .map(team => {
+                    // Initialize cumulativeRating for each team
+                    let cumulativeRating = 0;
+                    let membersWithRating = 0;
 
-                                return (
-                                    <div
-                                        key={`${team.tier.name}-${player.name}`}
-                                        className={`${isInactiveReserve ? "text-slate-500" : ""} m-1 grid grid-cols-3 gap-2`}
-                                    >
-                                        <div>
-                                            {player.name}{" "}
-                                            {isInactiveReserve ?
-                                                <span>(IR)</span>
-                                            :	""}{" "}
-                                            {team?.captain?.steam64Id === player.steam64Id ?
-                                                <GiPirateHat
-                                                    size="1.5em"
-                                                    className="inline"
-                                                />
-                                            :	""}
+                    // First, iterate through players to calculate cumulativeRating and membersWithRating
+                    team.players.forEach(player => {
+                        const playerWithStats = players.find(
+                            p => p.steam64Id === player.steam64Id,
+                        );
+                        const isInactiveReserve =
+                            playerWithStats?.type === PlayerTypes.INACTIVE_RESERVE;
+
+                        if (!isInactiveReserve) {
+                            if (playerWithStats?.stats?.rating) {
+                                cumulativeRating += playerWithStats.stats.rating;
+                                membersWithRating++;
+                            }
+                        }
+                    });
+
+                    // Calculate average rating
+                    const averageRating = membersWithRating > 0 ? (cumulativeRating / membersWithRating).toFixed(2) : "N/A";
+
+                    return (
+                        <div key={`${team.tier.name}`}>
+                            <div className="basis-1/4 mx-4 border-b-[1px] border-slate-700 text-center">
+                                <strong>{team.name}</strong>{" "}
+                                <span
+                                    className={`text-gray-400 italic text-${tiers?.find(item => item.tier.name === team.tier.name)?.tier.color ?? ""}-400`}
+                                >
+                                    {team.tier.name}{" "}
+                                </span>
+
+                                {/*average rating*/}
+                                <div className="flex float-right">
+                                    <BiStats size="1.5em" className="mr-1 text-orange-500" />{" " + averageRating}
+                                </div>
+                            </div>
+                            <div className="mx-4 px-2">
+                                {team.players.length > 0 ? team.players.map(player => {
+                                    const playerWithStats = players.find(
+                                        p => p.steam64Id === player.steam64Id,
+                                    );
+                                    const isInactiveReserve =
+                                        playerWithStats?.type === PlayerTypes.INACTIVE_RESERVE;
+
+                                    return (
+                                        <div
+                                            key={`${team.tier.name}-${player.name}`}
+                                            className={`${isInactiveReserve ? "text-slate-500" : ""} m-1 grid grid-cols-3 gap-2`}
+                                        >
+                                            <div>
+                                                {player.name}{" "}
+                                                {isInactiveReserve ?
+                                                    <span>(IR)</span>
+                                                    : ""}{" "}
+                                                {team?.captain?.steam64Id === player.steam64Id ?
+                                                    <GiPirateHat
+                                                        size="1.5em"
+                                                        className="inline"
+                                                    />
+                                                    : ""}
+                                            </div>
+                                            <div className="text-center">
+                                                {playerWithStats?.stats?.rating.toFixed(2)}
+                                            </div>
+                                            <div>{playerWithStats?.role}</div>
                                         </div>
-                                        <div className="text-center">
-                                            {playerWithStats?.stats?.rating.toFixed(2)}
-                                        </div>
-                                        <div>{playerWithStats?.role}</div>
-                                        {/* <div className="text-xs text-gray-500"> <span className="hidden md:contents"><Mmr player={player} />({((player.mmr/team.tier.mmrCap)*100).toFixed(1)}%)</span></div> */}
+                                    );
+                                }) : (
+                                    <div className="my-1 text-center italic text-gray-500">
+                                        No rostered players
                                     </div>
-                                );
-                            })
-                        :   <div className="my-1 text-center italic text-gray-500">
-                                No rostered players
-                            </div>}
+                                )}
+                            </div>
                         </div>
-                    </div>
-                ))}
+                    );
+                })}
         </div>
     );
 }
